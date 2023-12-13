ad59de53eb18c84d521edd1a283dc751708e435d
krilop <krilop.gamer@gmail.com>
2023-12-13 05:31:48 +0300

    add some style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import PersonCard from './Components/PersonCard';
-import peopleData from './Data/peopleData';
+import peopleData from './Data/peopleData.json';// Хочу, чтобы все данные хранились в json'ах. Почему? Потому что так принято в мире. +Говорят, что так потом проще данные обрабатывать
 import './App.css';
 
 const App = () => {
@@ -11,9 +11,12 @@ const App = () => {
         <div className="app">
             <Header />
             <main>
-                {peopleData.map(person => (
-                    <PersonCard key={person.id} person={person} />
-                ))}
+                <div className="person-cards">
+                    {peopleData.map(person => (
+                        <PersonCard key={person.id} person={person} />
+                    ))}
+                </div>
+
                 {/*если что это просто демонстрация че есть код в этом файле будет другой пример по ссылке в грппе*/}
             </main>
             <Footer />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,11 +7,17 @@ const Header = () => {
         <header className="app-header">
             <h1>Famous People Website</h1>
             <nav>
+
+                {/* Сокральный смысл использовать список я не понял, поэтому пока убираю
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
+                */}
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
             </nav>
         </header>
     );
