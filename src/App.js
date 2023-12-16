caf139dfb99805ff0a918269442bc13530d02e1e
krilop <krilop.gamer@gmail.com>
2023-12-17 00:46:50 +0300

    man of the day will automatically change every day

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 // src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import peopleData from './Data/peopleData.json';// Хочу, чтобы все данные хранились в json'ах. Почему? Потому что так принято в мире. +Говорят, что так потом проще данные обрабатывать
+import peopleData from './Data/peopleData.json';
 import './App.css';
 import Main from './Components/Main';
 
 const App = () => {
 
     const currentTimeMillis = Date.now();
+    const initialIndex = Math.floor(currentTimeMillis / (1000 * 60*60*24)) % 5;
+    const [selectedPerson, setSelectedPerson] = useState(peopleData[initialIndex]);//начальное значение
 
-// Преобразуем миллисекунды в дни и берем остаток от деления на 5
-    const dayIndex = Math.floor(currentTimeMillis / (1000 * 60 * 60 * 24)) % 5;
-// Получение данных выбранной персоны
-    const selectedPerson = peopleData[dayIndex];
-    return (
+        useEffect(() => {
+            const interval = setInterval(() => {
+                const currentTimeMillis = Date.now();
+                const minuteIndex = Math.floor(currentTimeMillis / (1000 * 60*60*24)) % 5;
+                setSelectedPerson(peopleData[minuteIndex]);
+            }, 1000); // Обновлять каждую секунду (в миллисекундах)
+
+            return () => clearInterval(interval); // Остановить интервал при размонтировании компонента
+        }, []);
+
+        return (
         <div className="app">
             <Header />
 
