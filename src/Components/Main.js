43e6c1a82011a9f804958d913d40c07d7b3fb139
Makarov <i.eshcho.live@gmail.com>
2023-12-16 17:15:33 +0300

    добавлен чел дня, удален peopleData.js, изменил передаваемые поля в PersonCard.js(-id тк не используется)

---
+++ src/Components/Main.js
@@ -0,0 +1,25 @@
+import './main.css';
+import React from "react";
+
+const Main = ({person})=>{
+    const {name, years, img, link} = person;
+
+    return(
+        <div className="cont">
+            <div className="pers-day-card">
+                <div className="class-text">
+                    <h2>
+                        {name}
+                    </h2>
+                    <p>Годы жизни: {years}</p>
+                </div>
+                <img src={img} className={"day-image"}/>
+                <a href = {link} target="_blank" rel="noopener noreferrer">
+                    Learn more
+                </a>
+
+            </div>
+        </div>
+    )
+}
+export default Main;
