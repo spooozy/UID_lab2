4f2135f8319619a3ef327199409c65825992975c
Liza <matalygaliza@gmail.com>
2023-12-18 21:33:44 +0300

    last changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,6 @@ const Header = () => {
 
     return (
         <>
-
             <header className="app-header">
                 <div className="nav">
                     <img
@@ -55,26 +54,6 @@ const Header = () => {
                     </div>
                 </div>
 
-                {isOpen && (
-                    <div className="dropdown_menu">
-                        <ul>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contacts">Contacts</a></li>
-                            <li><a href="#list">List</a></li>
-                            <li>
-                                <div className="lang_ch">
-                                    <img
-                                        src={lang}
-                                        height="30"
-                                        width="auto"
-                                        alt="lang"
-                                    />
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                )}
             </header>
         </>
     );
