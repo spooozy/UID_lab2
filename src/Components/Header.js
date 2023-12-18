5f904d4138dbba7f19fddb78696fec67b1c344b9
krilop <krilop.gamer@gmail.com>
2023-12-18 17:14:36 +0300

    fix header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,40 +4,15 @@ import './Header.css';
 import logo from '../Images/logo.png';
 import lang from '../Images/lang.png';
 import bars from '../Images/bars.png';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
 
-        const [menuVisible, setMenuVisible] = useState(false);
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
 
-        const toggleMenu = () => {
-            setMenuVisible(!menuVisible);
-        };
-        return (
-            <header class="app-header">
-                <nav>
-                    {
-                        <>
-                            <div class="menu_back"></div>
-                            <menu>
-                                <img
-                                    src={logo}
-                                    height="45"
-                                    width="auto"
-                                    alt="logo"
-
-                                />
-                                <ul class="links">
-                                    <li><Link to="/">Home</Link></li>
-                                    <li><a href="#about">About</a></li>
-                                    <li><a href="#search">Search</a></li>
-                                    <li><a href="#contact">Contact</a></li>
-                                    <li><Link to="/ListOfPerson">List of person</Link></li>
-                                </ul>
-                                <div class="lang_ch">
-                                    <img
-                                        className="globe"
-/*
     const [isOpen, setIsOpen] = useState(false);
 
     const handleToggle = () => {
@@ -46,7 +21,7 @@ const Header = () => {
 
     return (
         <>
-            <div className="menu_back"></div>
+
             <header className="app-header">
                 <div className="nav">
                     <img
@@ -65,14 +40,20 @@ const Header = () => {
                             height="30"
                             width="auto"
                             alt="lang"
+                            onClick={toggleMenu}
                         />
+                        {menuVisible && (
+                            <div className="lang-ch-menu">
+                                {/* Implement language change functionality here */}
+                                <button onClick={() => console.log('Кнопка 1 нажата')}>ru</button>
+                                <button onClick={() => console.log('Кнопка 2 нажата')}>en</button>
+                            </div>
+                        )}
                     </div>
                     <div className="toggle_btn" onClick={handleToggle}>
-                        <img className = "toggle_img" src = {bars} alt = "bars">
-                        </img>
+                        <img className="toggle_img" src={bars} alt="bars" />
                     </div>
                 </div>
-
 
                 {isOpen && (
                     <div className="dropdown_menu">
@@ -84,34 +65,10 @@ const Header = () => {
                             <li>
                                 <div className="lang_ch">
                                     <img
-
                                         src={lang}
                                         height="30"
                                         width="auto"
-                                        alt="lang"*/
-                                        onClick={toggleMenu}
-                                    />
-                                    {menuVisible && (
-                                        <div className="lang-ch-menu">
-                                            {/*ДОВЕСИТЬ ИЗМЕНЕНИЕ ЯЗЫКА ПО КЛИКУ*/}
-                                            <button onClick={() => console.log('Кнопка 1 нажата')}>ru</button>
-                                            <button onClick={() => console.log('Кнопка 2 нажата')}>en</button>
-                                        </div>
-                                    )}
-                                </div>
-                            </menu>
-                            <task_name>
-                                <p>СОЗДАТЕЛИ МЕЖДУНАРОДНЫХ КОРПОРАЦИЙ</p>
-                            </task_name>
-                        </>
-                    }
-
-                </nav>
-            </header>
-        );
-    };
-export default Header
-/*
+                                        alt="lang"
                                     />
                                 </div>
                             </li>
@@ -124,4 +81,3 @@ export default Header
 };
 
 export default Header;
-*/
